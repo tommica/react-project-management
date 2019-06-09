c66f34318bf87e499566d43b188aab8e943eef46
Tommi Carleman <me@tommcarleman.net>
2019-06-09 12:52:56 +0200

    Updates

---
+++ precache-manifest.51f1ac81e5335da28ef803413be3a814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b71ac1e5568655a15f8d3dded26c2b",
+    "url": "/react-project-management/index.html"
+  },
+  {
+    "revision": "3f691908dbf444f51fa3",
+    "url": "/react-project-management/static/css/main.3645b9dd.chunk.css"
+  },
+  {
+    "revision": "13a278d5d1ccf56e788e",
+    "url": "/react-project-management/static/js/2.5fb37bb2.chunk.js"
+  },
+  {
+    "revision": "3f691908dbf444f51fa3",
+    "url": "/react-project-management/static/js/main.712889d4.chunk.js"
+  },
+  {
+    "revision": "1f1008599fa4498153a4",
+    "url": "/react-project-management/static/js/runtime~main.bbbdf024.js"
+  },
+  {
+    "revision": "8ea091ee9ef86b41c04d5af95f8fd3e5",
+    "url": "/react-project-management/static/media/bg.8ea091ee.jpg"
+  }
+]);
